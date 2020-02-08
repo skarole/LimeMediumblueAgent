301c8639306a5bd5ff621dea8358da1cc4b57b8b
skarole <skarole@student.21-school.ru>
2020-02-08 07:36:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int ft_iterative_factorial(int nb)
+{
+  
+}
+
+int main()
+{
+  int i=20;
+  int j=5;
+  int *a; a = &i;
+  int *b; b = &j;
+  ft_ultimate_div_mod(a,b);
+  printf("%d, %d\n", *a, *b);
+  return 0;
+}
